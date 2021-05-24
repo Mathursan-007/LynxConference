6d9c0a1a41e63b9fb20ecbc280a20170970463cc
Mathursan-007 <63546506+Mathursan-007@users.noreply.github.com>
2021-05-24 13:24:49 +0530

    config commit

---
+++ back-end/config.js
@@ -0,0 +1,9 @@
+const CLOUD_NAME="lynxcloud"
+const API_KEY="829654995463379"
+const API_SECRET="YmeDCN3qgEo-rHLX8DXAV6-tKhs"
+const URI="mongodb+srv://admin:admin@first.yueye.mongodb.net/conferenceDB?retryWrites=true&w=majority"
+
+
+module.exports={
+    CLOUD_NAME,API_KEY,API_SECRET,URI
+}
