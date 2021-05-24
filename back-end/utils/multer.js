153369cb67573a771ad7cf7d48e2b006e832358a
Mathursan-007 <63546506+Mathursan-007@users.noreply.github.com>
2021-05-24 13:23:55 +0530

    utils commit

---
+++ back-end/utils/multer.js
@@ -0,0 +1,16 @@
+const multer = require('multer')
+const path = require('path')
+
+
+module.exports=multer({
+    storage:multer.diskStorage({}),
+    fileFilter:(req,file,cb)=>{
+        let ext=path.extname(file.originalname)
+          if(ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png" && ext !== ".pptx" && ext !== ".docx"){
+              cb(new Error("File type is not supported"), false);
+              return;
+          }
+        cb(null, true);
+    }
+})
+
