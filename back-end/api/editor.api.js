285b3c0e006ef4bc3c5286bd5cb3e8dacc223b36
arosh-segar <arosh.segar@gmail.com>
2021-05-25 22:24:58 +0530

    editor api committed

---
+++ back-end/api/editor.api.js
@@ -0,0 +1,31 @@
+const { saveKeynoteSpeaker , getAllRequests , saveWorkshop } = require('../dal/request.dao');
+
+
+const createKeynoteSpeaker = async ({type,status,details}) => {
+
+    const keynoteSpeaker = {
+        type,
+        status,
+        details
+    }
+
+    return await saveKeynoteSpeaker(keynoteSpeaker);
+}
+
+const createWorkshop = async ({type,status,details}) => {
+
+    const workshop = {
+        type,
+        status,
+        details
+    }
+
+    return await saveWorkshop(workshop);
+}
+
+//retrieving the request details of all requests from the dal
+const getRequests = async ()=>{
+    return await getAllRequests();
+}
+
+module.exports = { createKeynoteSpeaker , getRequests , createWorkshop };
