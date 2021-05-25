0fa4e875bd14534bae3135526919901d6997bdd1
arosh-segar <arosh.segar@gmail.com>
2021-05-25 22:25:26 +0530

    request dao committed

---
+++ back-end/dal/request.dao.js
@@ -0,0 +1,36 @@
+let request = null;
+const ObjectID = require('mongodb').ObjectId;
+
+setTimeout(() => {
+    request = require('./connection').db('conferenceDB').collection('request');
+},5000);
+
+//Adding a new keynote speaker to the database
+const saveKeynoteSpeaker = async({type,status,details})=>{
+
+    const result = await request.insertOne({type,status,details});
+    return result.ops[0];
+
+}
+
+//Adding a new workshop speaker to the database
+const saveWorkshop = async({type,status,details})=>{
+
+    const result = await request.insertOne({type,status,details});
+    return result.ops[0];
+
+}
+
+//retrieving all the requests records from the db
+const getAllRequests =async ()=>{
+
+    const results = await request.find({});
+    return results.toArray();
+
+}
+
+const updateStatus = async (id,status)=>{
+    return await request.updateOne({"_id":ObjectID(id)},{$set:{status:status}})
+}
+
+module.exports = { saveKeynoteSpeaker , getAllRequests , saveWorkshop , updateStatus };
